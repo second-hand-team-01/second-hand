a71ebebf103b8cc132778397cd50798f8c34ebe4
poco <godiva140@gmail.com>
2023-06-09 10:57:42 +0900

    Feat: button 컴포넌트 구현

---
+++ fe/src/components/button/Button.stories.ts
@@ -0,0 +1,17 @@
+import { Button } from './Button';
+
+export default {
+  component: Button,
+};
+
+export const Default = {
+  args: {
+    title: 'label',
+    shape: 'large',
+    state: 'active',
+    textAlign: 'center',
+    icon: 'arrowDown',
+    iconSize: '12px',
+    iconColor: 'systemDefault',
+  },
+};
