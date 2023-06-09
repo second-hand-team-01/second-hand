754b8c1bc44a936dba89f9dcace70429b3b3f6c5
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-09 11:46:32 +0900

    Chore: 프로젝트 초기 설정

---
+++ BE/src/test/java/CodeSquad/SecondHand/SecondHandApplicationTests.java
@@ -0,0 +1,13 @@
+package CodeSquad.SecondHand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
