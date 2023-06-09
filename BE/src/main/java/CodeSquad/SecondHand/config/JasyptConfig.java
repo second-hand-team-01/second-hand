86f6dfa8aba09bb14fcfb269b52361843d0a6f6d
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-09 18:58:37 +0900

    Feat: 민감 정보 암호화를 위한 JasyptConfig.java 생성

---
+++ BE/src/main/java/CodeSquad/SecondHand/config/JasyptConfig.java
@@ -0,0 +1,29 @@
+package CodeSquad.SecondHand.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile({"dev","real"})
+public class JasyptConfig {
+
+    @Value("${encryptor.key}")
+    private String password;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
