2ac3f431bbc2014af7af99835882aed4b2333027
poco <godiva140@gmail.com>
2023-06-09 21:07:31 +0900

    Feat: Layout 추가

--- fe/.storybook/preview.ts
+++ fe/.storybook/preview.ts
@@ -2,6 +2,7 @@ import { withThemeFromJSXProvider } from '@storybook/addon-styling';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle, theme } from '../src/styles/GlobalStyle';
 import '@styles/index.css';
+import './style.css';
 
 export const decorators = [
   withThemeFromJSXProvider({
