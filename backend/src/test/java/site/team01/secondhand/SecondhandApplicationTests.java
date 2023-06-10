363085cd5cd5d3dbfe5931f1ba9b22e601985876
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-11 02:13:09 +0900

    Chore: 패키지 명 수정에 따른 재설정

---
+++ backend/src/test/java/site/team01/secondhand/SecondhandApplicationTests.java
@@ -0,0 +1,13 @@
+package site.team01.secondhand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondhandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
