a03a64c54438690ac21638136a50d05a48818ebd
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-11 01:40:06 +0900

    Chore: SecondHandApplication.java 패키지 명 수정

--- BE/src/main/java/codesquad/secondhand/SecondHandApplication.java
+++ BE/src/main/java/codesquad/secondhand/SecondHandApplication.java
@@ -1,4 +1,4 @@
-package CodeSquad.SecondHand;
+package codesquad.secondhand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
