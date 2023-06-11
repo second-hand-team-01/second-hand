a6a5e05be9cf6e20a9b2a1c6b8036eda2fc34231
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-06-11 21:10:06 +0900

    feat: 화면 이동 버튼 구현
    - 원하는 View의 위치를 화면 정중앙으로 설정하는 메소드 구현
    - 화면이동 버튼을 생성하는 메소드 구현

--- iOS/SecondHand/SecondHand/ViewController.swift
+++ iOS/SecondHand/SecondHand/ViewController.swift
@@ -8,7 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-   override func viewDidLoad() {
-      super.viewDidLoad()
-   }
+    var detailButton: UIButton?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        setDetailButton()
+    }
+    
+    func setDetailButton() {
+        let buttonSize = CGSize(width: 200, height: 50)
+        detailButton = makeDetailButton(size: buttonSize)
+        detailButton?.frame.origin = getCenterPoint(of: detailButton)
+        addButtonToView(button: detailButton)
+    }
+    
+    func makeDetailButton(size: CGSize) -> UIButton {
+        var button = UIButton(frame: CGRect(origin: CGPoint(),
+                                            size: size))
+
+        button.setTitle("상세화면으로 이동", for: .normal)
+        button.backgroundColor = .lightGray
+        return button
+    }
+    
+    func addButtonToView(button: UIButton?) {
+        guard let button = button else {
+            return
+        }
+        self.view.addSubview(button)
+    }
+    
+    func getCenterPoint(of view: UIView?) -> CGPoint {
+        let screenframe = self.view.frame
+        guard let frame = view?.frame else { return CGPoint() }
+        let pointX = (screenframe.width / 2) - (frame.width / 2)
+        let pointY = (screenframe.height / 2) - (frame.height / 2)
+        
+        return CGPoint(x: pointX, y: pointY)
+    }
 }
