1e616e91e1d3fba8ebd10de54a50f787691acd26
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-06-12 21:01:39 +0900

    chore: gitignore 적용안되는 문제 해결

--- iOS/SecondHand/SecondHand/SceneDelegate.swift
+++ iOS/SecondHand/SecondHand/SceneDelegate.swift
@@ -9,19 +9,19 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-    
+
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
-               options connectionOptions: UIScene.ConnectionOptions) {
-        
+               options connectionOptions: UIScene.ConnectionOptions
+    ) {
+
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        
+
         let rootViewController = ViewController()
         let navigationController = UINavigationController(rootViewController: rootViewController)
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
-        
         self.window = window
     }
 }
