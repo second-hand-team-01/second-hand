6bebf47b52043f239c0798ae9dc4ba344ea86408
HansolWorld <hs9269@gmail.com>
2023-06-12 16:03:40 +0900

    feat: tabbar 추가

---
+++ iOS/SecondHand/SecondHand/ItemListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ItemListViewController.swift
+//  SecondHand
+//
+//  Created by apple on 2023/06/12.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- iOS/SecondHand/SecondHand/SceneDelegate.swift
+++ iOS/SecondHand/SecondHand/SceneDelegate.swift
@@ -8,5 +8,19 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-   var window: UIWindow?
+    var window: UIWindow?
+    
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let tabBarViewController = TabBarViewController()
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = tabBarViewController
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
 }
+
+
