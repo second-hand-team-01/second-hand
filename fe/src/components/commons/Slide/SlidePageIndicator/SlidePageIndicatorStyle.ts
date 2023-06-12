59ffd2ac8e2a36db6807e9dbc3db1baeaa411324
realsnoopso <real@snoop.so>
2023-06-12 20:38:32 +0900

    Design: Page Indicator 에 그라디언트 추가

--- fe/src/components/commons/Slide/SlidePageIndicator/SlidePageIndicatorStyle.ts
+++ fe/src/components/commons/Slide/SlidePageIndicator/SlidePageIndicatorStyle.ts
@@ -1,14 +1,21 @@
 import styled from 'styled-components';
 
 export const SlidePageIndicator = styled.div`
-  position: absolute;
-  bottom: 0;
   display: flex;
   z-index: 1;
   position: absolute;
-  bottom: 18px;
+  bottom: 0;
   left: 50%;
   transform: translateX(-50%);
+  background: linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0) 0%,
+    rgba(0, 0, 0, 0.4) 100%
+  );
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 72px;
 `;
 
 export const Element = styled.div`
