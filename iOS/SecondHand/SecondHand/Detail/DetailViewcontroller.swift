bfd44105b629ca43a193559f11b7b08b4330a700
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-06-13 12:17:50 +0900

    Style: layout 설정 코드 스타일 변경
    - 뷰 변화에 유연하도록 willlayoutSubviews에서 layout 설정
    - Lint Rule 적용을 위해 layout 설정 메소드 작게 분리

--- iOS/SecondHand/SecondHand/Detail/DetailViewcontroller.swift
+++ iOS/SecondHand/SecondHand/Detail/DetailViewcontroller.swift
@@ -8,53 +8,66 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-    var productImages = UIImageView()
+    var productImageView = UIImageView()
     var pageControl = UIPageControl()
     var sellerInfo = SellerInfo()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
-        sellerInfo.layer.cornerRadius = sellerInfo.frame.height / 2
-        productImages.backgroundColor = .green
-        pageControl.backgroundColor = .black
-        pageControl.backgroundColor = .yellow
-        sellerInfo.backgroundColor = .gray
+        setUI()
         sellerInfo.configure(name: "Woodasdfasdfsafasdfasdfasdfasdfasdfasdf")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
         layoutConstraint()
     }
     
+    private func setUI() {
+        self.view.backgroundColor = .white
+        sellerInfo.layer.cornerRadius = sellerInfo.frame.height / 2
+        productImageView.backgroundColor = .green
+        pageControl.backgroundColor = .yellow
+        sellerInfo.backgroundColor = .gray
+    }
+    
     private func layoutConstraint() {
-        // 1. 상품 이미지 Constraint 설정
-        self.view.addSubview(productImages)
-        productImages.translatesAutoresizingMaskIntoConstraints = false
-
+        layoutProductImageView()
+        layoutPageControl()
+        layoutSellerInfo()
+    }
+    
+    private func layoutProductImageView() {
+        self.view.addSubview(productImageView)
+        productImageView.translatesAutoresizingMaskIntoConstraints = false
         let safeArea = self.view.safeAreaLayoutGuide
         let heightRatio: CGFloat = 5 / 4
         NSLayoutConstraint.activate([
-            productImages.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
-            productImages.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
-            productImages.topAnchor.constraint(equalTo: self.view.topAnchor),
-            productImages.heightAnchor.constraint(equalTo: productImages.widthAnchor,
-                                              multiplier: heightRatio)
+            productImageView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            productImageView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            productImageView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            productImageView.heightAnchor.constraint(equalTo: productImageView.widthAnchor,
+                                                     multiplier: heightRatio)
         ])
-
-        // 2. 페이지 컨트롤 Constraint 설정
+    }
+    
+    private func layoutPageControl() {
         self.view.addSubview(pageControl)
         pageControl.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            pageControl.leadingAnchor.constraint(equalTo: self.productImages.leadingAnchor),
-            pageControl.trailingAnchor.constraint(equalTo: self.productImages.trailingAnchor),
-            pageControl.bottomAnchor.constraint(equalTo: productImages.bottomAnchor),
+            pageControl.leadingAnchor.constraint(equalTo: self.productImageView.leadingAnchor),
+            pageControl.trailingAnchor.constraint(equalTo: self.productImageView.trailingAnchor),
+            pageControl.bottomAnchor.constraint(equalTo: productImageView.bottomAnchor),
             pageControl.heightAnchor.constraint(equalToConstant: 20)
         ])
-
-        // 3. 판매자 정보 Constraint 설정
+    }
+    
+    private func layoutSellerInfo() {
         self.view.addSubview(sellerInfo)
         sellerInfo.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            sellerInfo.leadingAnchor.constraint(equalTo: productImages.leadingAnchor, constant: 16),
-            sellerInfo.trailingAnchor.constraint(equalTo: productImages.trailingAnchor, constant: -16),
+            sellerInfo.leadingAnchor.constraint(equalTo: productImageView.leadingAnchor, constant: 16),
+            sellerInfo.trailingAnchor.constraint(equalTo: productImageView.trailingAnchor, constant: -16),
             sellerInfo.topAnchor.constraint(equalTo: self.pageControl.bottomAnchor, constant: 16),
             sellerInfo.heightAnchor.constraint(equalToConstant: 60)
         ])
