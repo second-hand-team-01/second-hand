e14845130779814f28c6bc2a42e93a36d37f2622
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-06-14 00:11:31 +0900

    Feat: 탭바 컨트롤러 추가
    - 첫 화면 진입시 탭바 보이도록 추가

--- iOS/SecondHand/SecondHand/SceneDelegate.swift
+++ iOS/SecondHand/SecondHand/SceneDelegate.swift
@@ -14,15 +14,30 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions
     ) {
-
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let listViewController = UINavigationController(rootViewController: ViewController())
+        listViewController.tabBarItem = makeTabBarItem(titleName: "홈화면",
+                                                       imageName: "house",
+                                                       selectedImageName: "house.fill")
 
-        let tabBarController = UITabBarController()
+        var tabBarController = UITabBarController()
         tabBarController.setViewControllers([listViewController], animated: true)
+        
         window.rootViewController = tabBarController
         window.makeKeyAndVisible()
         self.window = window
+    }
+    
+    private func makeTabBarItem(titleName: String,
+                                imageName: String,
+                                selectedImageName: String)
+    -> UITabBarItem {
+        let tabBarItem = UITabBarItem()
+        tabBarItem.title = titleName
+        tabBarItem.image = UIImage(systemName: imageName)
+        tabBarItem.selectedImage = UIImage(systemName: selectedImageName)
+        
+        return tabBarItem
     }
 }
