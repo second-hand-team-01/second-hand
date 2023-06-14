1c10f44305861f1095bb8762159e2e45d05b62dd
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-06-14 14:51:38 +0900

    Design: 채팅, 관심, 조회수 레이블 추가
    - addSubviews: addSubview 추상화한 메소드 추가

--- iOS/SecondHand/SecondHand/Detail/DetailContentView+layoutConstraint.swift
+++ iOS/SecondHand/SecondHand/Detail/DetailContentView+layoutConstraint.swift
@@ -8,30 +8,44 @@
 import UIKit
 
 extension DetailConetntView {
+    func addSubviews() {
+        let subViews = [
+            productImageView,
+            pageControl,
+            sellerInfo,
+            statusButton,
+            titleLabel,
+            categoryLabel,
+            uploadTimeLabel,
+            descriptionLabel,
+            chatLabel,
+            chatCountLabel,
+            favoriteLabel,
+            favoriteCountLabel,
+            viewsLabel,
+            viewsCountLabel,
+        ]
+        
+        subViews.forEach {
+            self.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
     func layoutConstraint() {
-        self.addSubview(productImageView)
-        productImageView.translatesAutoresizingMaskIntoConstraints = false
         let heightRatio: CGFloat = 5 / 4
         NSLayoutConstraint.activate([
             productImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             productImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
             productImageView.topAnchor.constraint(equalTo: self.topAnchor),
             productImageView.heightAnchor.constraint(equalTo: productImageView.widthAnchor,
-                                                     multiplier: heightRatio)
-        ])
-        
-        self.addSubview(pageControl)
-        pageControl.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
+                                                     multiplier: heightRatio),
+            
             pageControl.leadingAnchor.constraint(equalTo: self.productImageView.leadingAnchor),
             pageControl.trailingAnchor.constraint(equalTo: self.productImageView.trailingAnchor),
             pageControl.bottomAnchor.constraint(equalTo: productImageView.bottomAnchor),
-            pageControl.heightAnchor.constraint(equalToConstant: 20)
-        ])
-        
-        self.addSubview(sellerInfo)
-        sellerInfo.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
+            pageControl.heightAnchor.constraint(equalToConstant: 20),
+            
             sellerInfo.leadingAnchor.constraint(equalTo: productImageView.leadingAnchor, constant: 16),
             sellerInfo.trailingAnchor.constraint(equalTo: productImageView.trailingAnchor, constant: -16),
             sellerInfo.topAnchor.constraint(equalTo: self.pageControl.bottomAnchor, constant: 16),
@@ -39,42 +53,43 @@ extension DetailConetntView {
         ])
         sellerInfo.layoutConstraint()
         
-        self.addSubview(statusButton)
-        statusButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             statusButton.leadingAnchor.constraint(equalTo: sellerInfo.leadingAnchor),
-            statusButton.topAnchor.constraint(equalTo: sellerInfo.bottomAnchor, constant: 16)
-        ])
-        
-        self.addSubview(titleLabel)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
+            statusButton.topAnchor.constraint(equalTo: sellerInfo.bottomAnchor, constant: 16),
+            
             titleLabel.leadingAnchor.constraint(equalTo: sellerInfo.leadingAnchor),
             titleLabel.trailingAnchor.constraint(lessThanOrEqualTo: sellerInfo.trailingAnchor),
-            titleLabel.topAnchor.constraint(equalTo: statusButton.bottomAnchor, constant: 16)
-        ])
-        
-        self.addSubview(categoryLabel)
-        categoryLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: statusButton.bottomAnchor, constant: 16),
+            
             categoryLabel.leadingAnchor.constraint(equalTo: sellerInfo.leadingAnchor),
-            categoryLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8)
-        ])
-        
-        self.addSubview(uploadTimeLabel)
-        uploadTimeLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
+            categoryLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            
             uploadTimeLabel.leadingAnchor.constraint(equalTo: categoryLabel.trailingAnchor, constant: 8),
             uploadTimeLabel.trailingAnchor.constraint(lessThanOrEqualTo: sellerInfo.trailingAnchor),
-            uploadTimeLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8)
-        ])
-        
-        self.addSubview(descriptionLabel)
-        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
+            uploadTimeLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            
             descriptionLabel.leadingAnchor.constraint(equalTo: sellerInfo.leadingAnchor),
             descriptionLabel.trailingAnchor.constraint(equalTo: sellerInfo.trailingAnchor),
-            descriptionLabel.topAnchor.constraint(equalTo: categoryLabel.bottomAnchor, constant: 16)
+            descriptionLabel.topAnchor.constraint(equalTo: categoryLabel.bottomAnchor, constant: 16),
+            
+            chatLabel.leadingAnchor.constraint(equalTo: sellerInfo.leadingAnchor),
+            chatLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 16),
+            
+            chatCountLabel.leadingAnchor.constraint(equalTo: chatLabel.trailingAnchor, constant: 4),
+            chatCountLabel.topAnchor.constraint(equalTo: chatLabel.topAnchor),
+            
+            favoriteLabel.leadingAnchor.constraint(equalTo: chatCountLabel.trailingAnchor, constant: 8),
+            favoriteLabel.topAnchor.constraint(equalTo: chatLabel.topAnchor),
+            
+            favoriteCountLabel.leadingAnchor.constraint(equalTo: favoriteLabel.trailingAnchor, constant: 4),
+            favoriteCountLabel.topAnchor.constraint(equalTo: chatLabel.topAnchor),
+            
+            viewsLabel.leadingAnchor.constraint(equalTo: favoriteCountLabel.trailingAnchor, constant: 8),
+            viewsLabel.topAnchor.constraint(equalTo: chatLabel.topAnchor),
+            
+            viewsCountLabel.leadingAnchor.constraint(equalTo: viewsLabel.trailingAnchor, constant: 4),
+            viewsCountLabel.topAnchor.constraint(equalTo: chatLabel.topAnchor),
+            viewsCountLabel.trailingAnchor.constraint(lessThanOrEqualTo: sellerInfo.trailingAnchor)
         ])
     }
 }
--- iOS/SecondHand/SecondHand/Detail/DetailContentView.swift
+++ iOS/SecondHand/SecondHand/Detail/DetailContentView.swift
@@ -16,8 +16,15 @@ class DetailConetntView: UIView {
     var categoryLabel = UILabel()
     var uploadTimeLabel = UILabel()
     var descriptionLabel = UILabel()
+    var chatLabel = UILabel()
+    var chatCountLabel = UILabel()
+    var favoriteLabel = UILabel()
+    var favoriteCountLabel = UILabel()
+    var viewsLabel = UILabel()
+    var viewsCountLabel = UILabel()
     
     override func layoutSubviews() {
+        addSubviews()
         layoutConstraint()
         setUI()
         statusButton = makeStatusButton()
@@ -30,6 +37,12 @@ class DetailConetntView: UIView {
         descriptionLabel.numberOfLines = 0
         descriptionLabel.text = "어린시절 추억의 향수를 불러 일으키는 롤러 스케이트입니다. 빈티지 특성상 사용감 있지만 전체적으로 깨끗한 상태입니다\n촬영용 소품이나, 거실에 장식용으로 추천해 드립니다. 단품 입고 되었습니다. 새 제품으로 보존된 제품으로 전용박스까지 보내드립니다.\n 사이즈는 235 입니다.\n fdjkgnsdfjkngsdfjknsgfkjnadfsgjnk"
         sellerInfo.configure(name: "Wood")
+        chatLabel.text = "채팅"
+        chatCountLabel.text = "0"
+        favoriteLabel.text = "관심"
+        favoriteCountLabel.text = "0"
+        viewsLabel.text = "조회"
+        viewsCountLabel.text = "1"
     }
     
     private func setUI() {
@@ -40,7 +53,7 @@ class DetailConetntView: UIView {
         statusButton = makeStatusButton()
     }
     
-    func makeStatusButton() -> UIButton {
+    private func makeStatusButton() -> UIButton {
         let button = UIButton(type: .system)
         button.setTitle("판매중", for: .normal)
         button.setImage(UIImage(systemName: "chevron.down"), for: .normal)
