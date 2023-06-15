f5546648aeb9684f8a5f385f9091ddad8037b930
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-16 03:27:14 +0900

    Feat: domain 클래스에서 @Slf4j 어노테이션 삭제

--- be/src/main/java/codesquad/secondhand/domain/Location.java
+++ be/src/main/java/codesquad/secondhand/domain/Location.java
@@ -1,28 +1,27 @@
-//package codesquad.secondhand.domain;
-//
-//import lombok.Getter;
-//import lombok.extern.slf4j.Slf4j;
-//
-//import javax.persistence.*;
-//
-//@Slf4j
-//@Getter
-//@Entity
-//@Table(name = "location")
-//public class Location {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "location_idx")
-//    private Long locationIdx;
-//
-//    @Column(name = "city", nullable = false)
-//    private String city;
-//
-//    @Column(name = "district", nullable = false)
-//    private String district;
-//
-//    @Column(name = "town", nullable = false)
-//    private String town;
-//
-//}
+package codesquad.secondhand.domain;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Table(name = "location")
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "location_idx")
+    private Long locationIdx;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "district", nullable = false)
+    private String district;
+
+    @Column(name = "town", nullable = false)
+    private String town;
+
+}
