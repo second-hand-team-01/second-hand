64004250c2421f2aa27da82294582a3783bee0eb
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-15 16:27:10 +0900

    Fix: schema.sql 수정을 통한 RDB 구조 개편 및 chat_room 테이블 추가

--- be/src/main/resources/schema.sql
+++ be/src/main/resources/schema.sql
@@ -7,9 +7,9 @@ DROP SCHEMA IF EXISTS `second-hand` ;
 CREATE SCHEMA IF NOT EXISTS `second-hand` DEFAULT CHARACTER SET utf8 ;
 USE `second-hand` ;
 
-DROP TABLE IF EXISTS `location` ;
+DROP TABLE IF EXISTS `second-hand`.`location` ;
 
-CREATE TABLE IF NOT EXISTS `location` (
+CREATE TABLE IF NOT EXISTS `second-hand`.`location` (
   `location_idx` BIGINT(10) NOT NULL AUTO_INCREMENT,
   `city` VARCHAR(45) NOT NULL,
   `district` VARCHAR(45) NOT NULL,
@@ -17,104 +17,145 @@ CREATE TABLE IF NOT EXISTS `location` (
   PRIMARY KEY (`location_idx`))
 ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `member` ;
+DROP TABLE IF EXISTS `second-hand`.`member` ;
 
-CREATE TABLE IF NOT EXISTS `member` (
+CREATE TABLE IF NOT EXISTS `second-hand`.`member` (
   `member_idx` BIGINT(10) NOT NULL AUTO_INCREMENT,
   `login_id` VARCHAR(45) NOT NULL,
-  `image_url` VARCHAR(45) NULL,
   `main_location_idx` BIGINT(10) NOT NULL,
   `sub_location_idx` BIGINT(10) NULL,
   PRIMARY KEY (`member_idx`),
-  UNIQUE INDEX `id_UNIQUE` (`login_id` ASC) VISIBLE,
-  INDEX `fk_member_location1_idx` (`main_location_idx` ASC) VISIBLE,
-  INDEX `fk_member_location2_idx` (`sub_location_idx` ASC) VISIBLE,
-  CONSTRAINT `fk_member_location1`
+  UNIQUE INDEX `login_id_UNIQUE` (`login_id` ASC) VISIBLE,
+  INDEX `fk_member_location3_idx` (`main_location_idx` ASC) VISIBLE,
+  INDEX `fk_member_location4_idx` (`sub_location_idx` ASC) VISIBLE,
+  CONSTRAINT `fk_member_location3`
     FOREIGN KEY (`main_location_idx`)
-    REFERENCES `location` (`location_idx`)
+    REFERENCES `second-hand`.`location` (`location_idx`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_member_location2`
+  CONSTRAINT `fk_member_location4`
     FOREIGN KEY (`sub_location_idx`)
-    REFERENCES `location` (`location_idx`)
+    REFERENCES `second-hand`.`location` (`location_idx`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `category` ;
+DROP TABLE IF EXISTS `second-hand`.`category` ;
 
-CREATE TABLE IF NOT EXISTS `category` (
+CREATE TABLE IF NOT EXISTS `second-hand`.`category` (
   `category_idx` BIGINT(10) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`category_idx`))
 ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `item` ;
+DROP TABLE IF EXISTS `second-hand`.`item` ;
 
-CREATE TABLE IF NOT EXISTS `item` (
+CREATE TABLE IF NOT EXISTS `second-hand`.`item` (
   `item_idx` BIGINT(10) NOT NULL AUTO_INCREMENT,
   `seller_idx` BIGINT(10) NOT NULL,
   `category_idx` BIGINT(10) NOT NULL,
   `location_idx` BIGINT(10) NOT NULL,
-  `main_image_url` VARCHAR(100) NULL,
+  `main_image_idx` BIGINT(10) NULL,
   `name` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(500) NULL,
   `price` INT NOT NULL,
-  `view` INT NOT NULL,
-  `status` VARCHAR(20) NOT NULL DEFAULT '판매중',
+  `view` INT NOT NULL DEFAULT 0,
+  `status` VARCHAR(45) NOT NULL DEFAULT '판매중',
   PRIMARY KEY (`item_idx`),
-  INDEX `fk_item_member1_idx` (`seller_idx` ASC) VISIBLE,
-  INDEX `fk_item_location1_idx` (`location_idx` ASC) VISIBLE,
-  INDEX `fk_item_category1_idx` (`category_idx` ASC) VISIBLE,
-  CONSTRAINT `fk_item_member1`
+  INDEX `fk_item_member2_idx` (`seller_idx` ASC) VISIBLE,
+  INDEX `fk_item_category2_idx` (`category_idx` ASC) VISIBLE,
+  INDEX `fk_item_location2_idx` (`location_idx` ASC) VISIBLE,
+  INDEX `fk_item_item_image1_idx` (`main_image_idx` ASC) VISIBLE,
+  CONSTRAINT `fk_item_member2`
     FOREIGN KEY (`seller_idx`)
-    REFERENCES `member` (`member_idx`)
+    REFERENCES `second-hand`.`member` (`member_idx`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_item_location1`
-    FOREIGN KEY (`location_idx`)
-    REFERENCES `location` (`location_idx`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_item_category1`
+  CONSTRAINT `fk_item_category2`
     FOREIGN KEY (`category_idx`)
-    REFERENCES `category` (`category_idx`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-DROP TABLE IF EXISTS `interest` ;
-
-CREATE TABLE IF NOT EXISTS `interest` (
-  `interest_idx` BIGINT(10) NOT NULL AUTO_INCREMENT,
-  `member_idx` BIGINT(10) NOT NULL,
-  `item_idx` BIGINT(10) NOT NULL,
-  INDEX `fk_member_has_item_item1_idx` (`item_idx` ASC) VISIBLE,
-  INDEX `fk_member_has_item_member1_idx` (`member_idx` ASC) VISIBLE,
-  PRIMARY KEY (`interest_idx`),
-  CONSTRAINT `fk_member_has_item_member1`
-    FOREIGN KEY (`member_idx`)
-    REFERENCES `member` (`member_idx`)
+    REFERENCES `second-hand`.`category` (`category_idx`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_member_has_item_item1`
-    FOREIGN KEY (`item_idx`)
-    REFERENCES `item` (`item_idx`)
+  CONSTRAINT `fk_item_location2`
+    FOREIGN KEY (`location_idx`)
+    REFERENCES `second-hand`.`location` (`location_idx`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_item_item_image1`
+    FOREIGN KEY (`main_image_idx`)
+    REFERENCES `second-hand`.`item_image` (`item_image_idx`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `item_image` ;
+DROP TABLE IF EXISTS `second-hand`.`item_image` ;
 
-CREATE TABLE IF NOT EXISTS `item_image` (
+CREATE TABLE IF NOT EXISTS `second-hand`.`item_image` (
   `item_image_idx` BIGINT(10) NOT NULL AUTO_INCREMENT,
   `item_idx` BIGINT(10) NOT NULL,
-  `image_url` VARCHAR(100) NULL,
+  `image_url` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`item_image_idx`),
-  INDEX `fk_item_image_item1_idx` (`item_idx` ASC) VISIBLE,
-  CONSTRAINT `fk_item_image_item1`
+  INDEX `fk_item_image_item2_idx` (`item_idx` ASC) VISIBLE,
+  CONSTRAINT `fk_item_image_item2`
     FOREIGN KEY (`item_idx`)
-    REFERENCES `item` (`item_idx`)
+    REFERENCES `second-hand`.`item` (`item_idx`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS `second-hand`.`member_image` ;
+
+CREATE TABLE IF NOT EXISTS `second-hand`.`member_image` (
+  `member_image_idx` BIGINT(10) NOT NULL AUTO_INCREMENT,
+  `member_idx` BIGINT(10) NOT NULL,
+  `image_url` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`member_image_idx`),
+  INDEX `fk_member_image_member1_idx` (`member_idx` ASC) VISIBLE,
+  UNIQUE INDEX `member_idx_UNIQUE` (`member_idx` ASC) VISIBLE,
+  CONSTRAINT `fk_member_image_member1`
+    FOREIGN KEY (`member_idx`)
+    REFERENCES `second-hand`.`member` (`member_idx`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS `second-hand`.`interest` ;
+
+CREATE TABLE IF NOT EXISTS `second-hand`.`interest` (
+  `interest_idx` BIGINT(10) NOT NULL,
+  `member_idx` BIGINT(10) NOT NULL,
+  `item_idx` BIGINT(10) NOT NULL,
+  INDEX `fk_member_has_item_item2_idx` (`item_idx` ASC) VISIBLE,
+  INDEX `fk_member_has_item_member2_idx` (`member_idx` ASC) VISIBLE,
+  PRIMARY KEY (`interest_idx`),
+  CONSTRAINT `fk_member_has_item_member2`
+    FOREIGN KEY (`member_idx`)
+    REFERENCES `second-hand`.`member` (`member_idx`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_member_has_item_item2`
+    FOREIGN KEY (`item_idx`)
+    REFERENCES `second-hand`.`item` (`item_idx`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS `second-hand`.`chat_room` ;
+
+CREATE TABLE IF NOT EXISTS `second-hand`.`chat_room` (
+  `chat_room_idx` BIGINT(10) NOT NULL AUTO_INCREMENT,
+  `item_idx` BIGINT(10) NOT NULL,
+  `buyer_idx` BIGINT(10) NOT NULL,
+  INDEX `fk_member_has_item_item3_idx` (`item_idx` ASC) VISIBLE,
+  INDEX `fk_member_has_item_member3_idx` (`buyer_idx` ASC) VISIBLE,
+  PRIMARY KEY (`chat_room_idx`),
+  CONSTRAINT `fk_member_has_item_member3`
+    FOREIGN KEY (`buyer_idx`)
+    REFERENCES `second-hand`.`member` (`member_idx`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_member_has_item_item3`
+    FOREIGN KEY (`item_idx`)
+    REFERENCES `second-hand`.`item` (`item_idx`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
