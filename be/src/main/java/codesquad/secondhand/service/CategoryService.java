daa42e8c92c31ef70a3262cc127c2206f6f8d0be
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-16 00:17:23 +0900

    Refactor: datasource가 연결되지 않은 상황에서 에러 발생 방지를 위한 주석 처리

--- be/src/main/java/codesquad/secondhand/service/CategoryService.java
+++ be/src/main/java/codesquad/secondhand/service/CategoryService.java
@@ -1,29 +1,29 @@
-package codesquad.secondhand.service;
-
-import codesquad.secondhand.domain.Category;
-import codesquad.secondhand.dto.category.CategoryDto;
-import codesquad.secondhand.repository.CategoryRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Slf4j
-@Service
-@Transactional
-@RequiredArgsConstructor
-public class CategoryService {
-
-    private final CategoryRepository categoryRepository;
-
-    public List<CategoryDto> showAllCategories() {
-        log.info("[CategoryRepository.showAllCategories]");
-        List<Category> categories = categoryRepository.findAll();
-        return categories.stream()
-                .map(CategoryDto::of)
-                .collect(Collectors.toList());
-    }
-}
+//package codesquad.secondhand.service;
+//
+//import codesquad.secondhand.domain.Category;
+//import codesquad.secondhand.dto.category.CategoryDto;
+//import codesquad.secondhand.repository.CategoryRepository;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//@Slf4j
+//@Service
+//@Transactional
+//@RequiredArgsConstructor
+//public class CategoryService {
+//
+//    private final CategoryRepository categoryRepository;
+//
+//    public List<CategoryDto> showAllCategories() {
+//        log.info("[CategoryRepository.showAllCategories]");
+//        List<Category> categories = categoryRepository.findAll();
+//        return categories.stream()
+//                .map(CategoryDto::of)
+//                .collect(Collectors.toList());
+//    }
+//}
