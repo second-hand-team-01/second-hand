fab0d764bb466fc46843ff63ebcab33bff2660e4
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-15 16:19:25 +0900

    Feat: Location.java 필드 추가

--- be/src/main/java/codesquad/secondhand/domain/Location.java
+++ be/src/main/java/codesquad/secondhand/domain/Location.java
@@ -1,4 +1,22 @@
 package codesquad.secondhand.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "location")
 public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "location_idx")
+    private Long locationIdx;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "district")
+    private String district;
+
+    @Column(name = "town")
+    private String town;
 }
