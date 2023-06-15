cd1f842d311fd06db5f622efdf29177565c85aaf
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-16 03:28:44 +0900

    Feat: MemberService 생성

---
+++ be/src/main/java/codesquad/secondhand/service/MemberService.java
@@ -0,0 +1,17 @@
+package codesquad.secondhand.service;
+
+import codesquad.secondhand.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+}
