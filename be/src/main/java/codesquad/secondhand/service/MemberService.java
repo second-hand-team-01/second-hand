bcc3f30276273579b56ff57f75fd36f1f5918361
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-16 04:22:15 +0900

    Feat: 로그인 한 사용자의 주 지역 정보와 부 지역 정보를 가져오는 메서드 구현

--- be/src/main/java/codesquad/secondhand/service/MemberService.java
+++ be/src/main/java/codesquad/secondhand/service/MemberService.java
@@ -1,5 +1,7 @@
 package codesquad.secondhand.service;
 
+import codesquad.secondhand.domain.Member;
+import codesquad.secondhand.dto.location.LocationTownDto;
 import codesquad.secondhand.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,4 +16,14 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    public LocationTownDto getMainLocation(String memberLoginId) {
+        Member member = memberRepository.findByLoginId(memberLoginId);
+        return LocationTownDto.of(member.getMainLocation());
+    }
+
+    public LocationTownDto getSubLocation(String memberLoginId) {
+        log.info("[MemberService.getSubLocation]");
+        Member member = memberRepository.findByLoginId(memberLoginId);
+        return LocationTownDto.of(member.getSubLocation());
+    }
 }
