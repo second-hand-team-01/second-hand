5e5ddd1a7e221a4faabaefaf93e9956be90bb76e
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-16 03:13:18 +0900

    Refactor: datasource가 연결되지 않은 상황에서 에러 발생 방지를 위한 주석 처리

--- be/src/main/java/codesquad/secondhand/controller/LocationController.java
+++ be/src/main/java/codesquad/secondhand/controller/LocationController.java
@@ -1,33 +1,33 @@
-package codesquad.secondhand.controller;
-
-import codesquad.secondhand.dto.ResponseDto;
-import codesquad.secondhand.dto.StatusCode;
-import codesquad.secondhand.dto.location.LocationDto;
-import codesquad.secondhand.service.LocationService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-import static codesquad.secondhand.dto.StatusCode.RESPONSE_SUCCESS;
-
-@Slf4j
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/locations")
-public class LocationController {
-
-    private final LocationService locationService;
-
-    @GetMapping
-    public ResponseEntity<ResponseDto<List<LocationDto>>> showLocations() {
-        log.info("[LocationController.showLocations]");
-        List<LocationDto> locationDtos = locationService.showAllLocations();
-        return ResponseEntity.ok(ResponseDto.of(RESPONSE_SUCCESS, locationDtos));
-    }
-
-}
+//package codesquad.secondhand.controller;
+//
+//import codesquad.secondhand.dto.ResponseDto;
+//import codesquad.secondhand.dto.StatusCode;
+//import codesquad.secondhand.dto.location.LocationDto;
+//import codesquad.secondhand.service.LocationService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//import static codesquad.secondhand.dto.StatusCode.RESPONSE_SUCCESS;
+//
+//@Slf4j
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/locations")
+//public class LocationController {
+//
+//    private final LocationService locationService;
+//
+//    @GetMapping
+//    public ResponseEntity<ResponseDto<List<LocationDto>>> showLocations() {
+//        log.info("[LocationController.showLocations]");
+//        List<LocationDto> locationDtos = locationService.showAllLocations();
+//        return ResponseEntity.ok(ResponseDto.of(RESPONSE_SUCCESS, locationDtos));
+//    }
+//
+//}
