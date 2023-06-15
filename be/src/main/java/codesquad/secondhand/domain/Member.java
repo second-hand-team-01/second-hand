316e6db55f97f4dc5e69dc776d0845adb674c5bd
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-15 17:00:31 +0900

    Feat: Member.java 어노테이션 및 필드 추가

--- be/src/main/java/codesquad/secondhand/domain/Member.java
+++ be/src/main/java/codesquad/secondhand/domain/Member.java
@@ -1,4 +1,18 @@
 package codesquad.secondhand.domain;
 
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+
+@Slf4j
+@Getter
+@Entity
+@Table(name = "member")
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_idx")
+    private Long memberIdx;
 }
