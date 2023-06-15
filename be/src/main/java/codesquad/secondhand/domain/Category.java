7b141974824342168413ca51adb10d1549db46b4
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-15 16:18:42 +0900

    Feat: Category.java 필드 추가

--- be/src/main/java/codesquad/secondhand/domain/Category.java
+++ be/src/main/java/codesquad/secondhand/domain/Category.java
@@ -1,4 +1,16 @@
 package codesquad.secondhand.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_idx")
+    private Long categoryIdx;
+
+    @Column(name = "name", nullable = false)
+    private String name;
 }
