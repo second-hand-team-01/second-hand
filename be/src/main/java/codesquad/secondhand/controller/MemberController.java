16ab434ffd451515eb9af9fcb2c738251f5c10f7
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-16 15:43:25 +0900

    Refactor: datasource가 연결되지 않은 상황에서 에러 발생 방지를 위한 주석 처리

--- be/src/main/java/codesquad/secondhand/controller/MemberController.java
+++ be/src/main/java/codesquad/secondhand/controller/MemberController.java
@@ -1,31 +1,31 @@
-package codesquad.secondhand.controller;
-
-import codesquad.secondhand.dto.ResponseDto;
-import codesquad.secondhand.dto.location.MainSubTownDto;
-import codesquad.secondhand.service.MemberService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import static codesquad.secondhand.dto.StatusCode.RESPONSE_SUCCESS;
-
-@Slf4j
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/{memberLoginId}")
-public class MemberController {
-
-    private final MemberService memberService;
-
-    @GetMapping("/location")
-    public ResponseEntity<ResponseDto<MainSubTownDto>> showMemberLocations(@PathVariable String memberLoginId) {
-        log.info("[MemberController.showMemberLocations]");
-        MainSubTownDto mainSubTownDto = memberService.getMainSubLocation(memberLoginId);
-        return ResponseEntity.ok(ResponseDto.of(RESPONSE_SUCCESS, mainSubTownDto));
-    }
-
-}
+//package codesquad.secondhand.controller;
+//
+//import codesquad.secondhand.dto.ResponseDto;
+//import codesquad.secondhand.dto.location.MainSubTownDto;
+//import codesquad.secondhand.service.MemberService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import static codesquad.secondhand.dto.StatusCode.RESPONSE_SUCCESS;
+//
+//@Slf4j
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/{memberLoginId}")
+//public class MemberController {
+//
+//    private final MemberService memberService;
+//
+//    @GetMapping("/location")
+//    public ResponseEntity<ResponseDto<MainSubTownDto>> showMemberLocations(@PathVariable String memberLoginId) {
+//        log.info("[MemberController.showMemberLocations]");
+//        MainSubTownDto mainSubTownDto = memberService.getMainSubLocation(memberLoginId);
+//        return ResponseEntity.ok(ResponseDto.of(RESPONSE_SUCCESS, mainSubTownDto));
+//    }
+//
+//}
