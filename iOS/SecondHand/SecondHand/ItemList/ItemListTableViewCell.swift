8648faa0ba754860a6afca40a86867777c6a187e
HansolWorld <hs9269@gmail.com>
2023-06-16 13:31:55 +0900

    view: layout 함수 분리

--- iOS/SecondHand/SecondHand/ItemList/ItemListTableViewCell.swift
+++ iOS/SecondHand/SecondHand/ItemList/ItemListTableViewCell.swift
@@ -43,7 +43,6 @@ class ItemListTableViewCell: UITableViewCell {
         return label
     }()
     
-    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.configureItemListTableViewCell()
@@ -72,57 +71,83 @@ class ItemListTableViewCell: UITableViewCell {
         }
     }
     
-
     private func layoutItemListTableViewCell() {
-        thumbnailImage.translatesAutoresizingMaskIntoConstraints = false
+        self.layoutTitleLabel()
+        self.layoutLocationLabel()
+        self.layoutWriteTimeLabel()
+        self.layoutStateLabel()
+        self.layoutPriceLabel()
+        self.layoutThumbnailImage()
+        self.layoutTrailingLabel()
+    }
+    
+    private func layoutTitleLabel() {
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        locationLabel.translatesAutoresizingMaskIntoConstraints = false
-        writeTimeLabel.translatesAutoresizingMaskIntoConstraints = false
-        stateLabel.translatesAutoresizingMaskIntoConstraints = false
-        priceLabel.translatesAutoresizingMaskIntoConstraints = false
-        commentSymbolLabel.translatesAutoresizingMaskIntoConstraints = false
-        commentCountLabel.translatesAutoresizingMaskIntoConstraints = false
-        likeSymbolLabel.translatesAutoresizingMaskIntoConstraints = false
-        likeCountLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        NSLayoutConstraint.activate([
-            thumbnailImage.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 15),
-            thumbnailImage.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 16),
-//            thumbnailImage.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: 15),
-            thumbnailImage.widthAnchor.constraint(equalToConstant: 120),
-            thumbnailImage.heightAnchor.constraint(equalToConstant: 120)
-        ])
-  
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 19),
             titleLabel.leadingAnchor.constraint(equalTo: self.thumbnailImage.trailingAnchor, constant: 15),
             titleLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
             titleLabel.heightAnchor.constraint(equalToConstant: 22)
         ])
-
+    }
+    
+    private func layoutLocationLabel() {
+        locationLabel.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
             locationLabel.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 4),
             locationLabel.leadingAnchor.constraint(equalTo: self.thumbnailImage.trailingAnchor, constant: 15),
             locationLabel.heightAnchor.constraint(equalToConstant: 20)
         ])
-
+    }
+    
+    private func layoutWriteTimeLabel() {
+        writeTimeLabel.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
             writeTimeLabel.centerYAnchor.constraint(equalTo: self.locationLabel.centerYAnchor),
             writeTimeLabel.leadingAnchor.constraint(equalTo: self.locationLabel.trailingAnchor, constant: 13),
             writeTimeLabel.heightAnchor.constraint(equalToConstant: 20)
         ])
+    }
+    
+    private func layoutStateLabel() {
+        stateLabel.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             stateLabel.topAnchor.constraint(equalTo: self.locationLabel.bottomAnchor, constant: 4),
             stateLabel.leadingAnchor.constraint(equalTo: self.thumbnailImage.trailingAnchor, constant: 15),
             stateLabel.heightAnchor.constraint(equalToConstant: 22)
         ])
-        
+    }
+    
+    private func layoutPriceLabel() {
+        priceLabel.translatesAutoresizingMaskIntoConstraints = false
+
         NSLayoutConstraint.activate([
             priceLabel.topAnchor.constraint(equalTo: self.locationLabel.bottomAnchor, constant: 4),
             priceLabel.leadingAnchor.constraint(equalTo: self.stateLabel.trailingAnchor, constant: 4),
             priceLabel.heightAnchor.constraint(equalToConstant: 22)
         ])
+    }
+    
+    private func layoutThumbnailImage() {
+        thumbnailImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            thumbnailImage.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 15),
+            thumbnailImage.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 16),
+            thumbnailImage.widthAnchor.constraint(equalToConstant: 120),
+            thumbnailImage.heightAnchor.constraint(equalToConstant: 120)
+        ])
+    }
+    
+    private func layoutTrailingLabel() {
+        commentSymbolLabel.translatesAutoresizingMaskIntoConstraints = false
+        commentCountLabel.translatesAutoresizingMaskIntoConstraints = false
+        likeSymbolLabel.translatesAutoresizingMaskIntoConstraints = false
+        likeCountLabel.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             likeCountLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -21),
