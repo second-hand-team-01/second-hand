d38af30ea1f5c0ceca55ce31110136cd2f504869
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-16 15:39:15 +0900

    Remove: TODO 제거

--- be/src/main/java/codesquad/secondhand/service/LocationService.java
+++ be/src/main/java/codesquad/secondhand/service/LocationService.java
@@ -1,33 +1,30 @@
-//package codesquad.secondhand.service;
-//
-//import codesquad.secondhand.domain.Location;
-//import codesquad.secondhand.dto.location.LocationDto;
-//import codesquad.secondhand.repository.LocationRepository;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Slf4j
-//@Service
-//@Transactional
-//@RequiredArgsConstructor
-//public class LocationService {
-//
-//    private final LocationRepository locationRepository;
-//
-//    public List<LocationDto> showAllLocations() {
-//
-//        // TODO: 로그인 한 사용자의 mainLocationIdx와 subLocationIdx는 제외하고 List로 만들어줘야 함
-//
-//        log.info("[LocationService.showAllLocations]");
-//        List<Location> locations = locationRepository.findAll();
-//        return locations.stream()
-//                .map(LocationDto::of)
-//                .collect(Collectors.toList());
-//    }
-//
-//}
+package codesquad.secondhand.service;
+
+import codesquad.secondhand.domain.Location;
+import codesquad.secondhand.dto.location.LocationDto;
+import codesquad.secondhand.repository.LocationRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LocationService {
+
+    private final LocationRepository locationRepository;
+
+    public List<LocationDto> showAllLocations() {
+        log.info("[LocationService.showAllLocations]");
+        List<Location> locations = locationRepository.findAll();
+        return locations.stream()
+                .map(LocationDto::of)
+                .collect(Collectors.toList());
+    }
+
+}
