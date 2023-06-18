72e50ddd3666ec509ceebb7d920de3443fb2c05f
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-18 22:42:29 +0900

    Refactor: 통일성을 위해 기존 @Autowired 제거 뒤 @RequiredArgsConstructor를 사용한 생성자 의존성 주입

--- be/src/main/java/codesquad/secondhand/service/ItemService.java
+++ be/src/main/java/codesquad/secondhand/service/ItemService.java
@@ -3,18 +3,17 @@ package codesquad.secondhand.service;
 import codesquad.secondhand.domain.Item;
 import codesquad.secondhand.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class ItemService {
 
     private final ItemRepository itemRepository;
-
-    @Autowired
-    public ItemService(ItemRepository itemRepository) {
-        this.itemRepository = itemRepository;
-    }
 
 }
