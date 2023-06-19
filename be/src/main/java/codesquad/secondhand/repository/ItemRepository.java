4ce4c1505a0c1059e8745575b3c482ebcde03ae6
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-19 15:00:13 +0900

    Feat: ItemRepository에 지역 번호로 물품을 조회하는 기능 추가

--- be/src/main/java/codesquad/secondhand/repository/ItemRepository.java
+++ be/src/main/java/codesquad/secondhand/repository/ItemRepository.java
@@ -3,5 +3,9 @@ package codesquad.secondhand.repository;
 import codesquad.secondhand.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByLocationLocationIdx(Long locationIdx);
 }
