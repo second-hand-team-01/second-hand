a5f517c66dbeaf0a92b07eaee1ae618d05893010
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-19 15:01:00 +0900

    Feat: ItemService에 해당 지역에 속한 물품들을 출력하는 기능 추가

--- be/src/main/java/codesquad/secondhand/service/ItemService.java
+++ be/src/main/java/codesquad/secondhand/service/ItemService.java
@@ -1,10 +1,17 @@
 package codesquad.secondhand.service;
 
+import codesquad.secondhand.dto.item.ItemDto;
+import codesquad.secondhand.entity.Item;
+import codesquad.secondhand.repository.ChatRoomRepository;
+import codesquad.secondhand.repository.InterestRepository;
 import codesquad.secondhand.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -13,5 +20,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class ItemService {
 
     private final ItemRepository itemRepository;
+    private final ChatRoomRepository chatRoomRepository;
+    private final InterestRepository interestRepository;
+
+    public List<ItemDto> showItems(Long locationIdx) {
+        log.info("[ItemService.showItems()]");
+        List<Item> items = itemRepository.findByLocationLocationIdx(locationIdx);
+        return items.stream()
+                .map(item -> {
+                    String imageUrl = null;
+                    if (item.getItemImage() != null) {
+                        imageUrl = item.getItemImage().getImageUrl();
+                    }
+                    return ItemDto.of(item, imageUrl, chatRoomRepository.countByItem(item), interestRepository.countByItem(item));
+                })
+                .collect(Collectors.toList());
+    }
 
 }
