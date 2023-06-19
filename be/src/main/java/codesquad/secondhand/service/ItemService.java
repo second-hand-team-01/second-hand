f1cfea787b15528c13671f6031a38e52291e6e78
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-19 16:58:22 +0900

    Feat: Slice를 통한 페이지네이션 구현 및 기존 imageUrl이 null일 경우 실행되던 로직 제거

--- be/src/main/java/codesquad/secondhand/service/ItemService.java
+++ be/src/main/java/codesquad/secondhand/service/ItemService.java
@@ -1,12 +1,15 @@
 package codesquad.secondhand.service;
 
 import codesquad.secondhand.dto.item.ItemDto;
+import codesquad.secondhand.dto.item.ItemSliceDto;
 import codesquad.secondhand.entity.Item;
 import codesquad.secondhand.repository.ChatRoomRepository;
 import codesquad.secondhand.repository.InterestRepository;
 import codesquad.secondhand.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,18 +26,16 @@ public class ItemService {
     private final ChatRoomRepository chatRoomRepository;
     private final InterestRepository interestRepository;
 
-    public List<ItemDto> showItems(Long locationIdx) {
+    public ItemSliceDto showItems(Long locationIdx, Pageable pageable) {
         log.info("[ItemService.showItems()]");
-        List<Item> items = itemRepository.findByLocationLocationIdx(locationIdx);
-        return items.stream()
-                .map(item -> {
-                    String imageUrl = null;
-                    if (item.getItemImage() != null) {
-                        imageUrl = item.getItemImage().getImageUrl();
-                    }
-                    return ItemDto.of(item, imageUrl, chatRoomRepository.countByItem(item), interestRepository.countByItem(item));
-                })
+
+        Slice<Item> itemSlice = itemRepository.findByLocationLocationIdx(locationIdx, pageable);
+
+        List<ItemDto> itemDtos = itemSlice.getContent().stream()
+                .map(item -> ItemDto.of(item, chatRoomRepository.countByItem(item), interestRepository.countByItem(item)))
                 .collect(Collectors.toList());
+
+        return new ItemSliceDto(itemSlice.hasNext(), itemDtos);
     }
 
 }
