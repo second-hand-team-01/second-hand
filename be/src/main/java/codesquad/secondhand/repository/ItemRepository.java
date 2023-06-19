2535d74d156a41c24ddbf5a8ba0d26c7a08aa029
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-19 16:52:02 +0900

    Feat: 페이지네이션 구현을 위한 Pageable 매개 변수 추가

--- be/src/main/java/codesquad/secondhand/repository/ItemRepository.java
+++ be/src/main/java/codesquad/secondhand/repository/ItemRepository.java
@@ -1,11 +1,11 @@
 package codesquad.secondhand.repository;
 
 import codesquad.secondhand.entity.Item;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
-    List<Item> findByLocationLocationIdx(Long locationIdx);
+    Slice<Item> findByLocationLocationIdx(Long locationIdx, Pageable pageable);
 }
