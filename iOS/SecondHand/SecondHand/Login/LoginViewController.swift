31077db0502c55f945448510e08c3287f6d0ea7f
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-06-20 12:41:34 +0900

    Feat: 탭바에 로그인 화면 추가
    - 로그인에 필요한 탭바 아이템 추가
    - 로그인 뷰 컨트롤러 생성

---
+++ iOS/SecondHand/SecondHand/Login/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  SecondHand
+//
+//  Created by Wood on 2023/06/20.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.title = "내 계정"
+    }
+}
