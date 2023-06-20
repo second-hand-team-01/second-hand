afa295515b460afdd3cc31f9ee9e77552b22f619
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-21 04:02:53 +0900

    Feat: 로그인 하려는 사용자를 검증하고 jwt 토큰을 생성하는 createToken() 메서드 추가

--- be/src/main/java/codesquad/secondhand/service/MemberService.java
+++ be/src/main/java/codesquad/secondhand/service/MemberService.java
@@ -1,8 +1,11 @@
 package codesquad.secondhand.service;
 
+import codesquad.secondhand.dto.member.LoginRequestDto;
+import codesquad.secondhand.dto.member.MemberLoginIdMainSubDto;
 import codesquad.secondhand.entity.Member;
 import codesquad.secondhand.dto.location.LocationTownDto;
 import codesquad.secondhand.dto.location.MainSubTownDto;
+import codesquad.secondhand.jwt.JwtTokenProvider;
 import codesquad.secondhand.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,22 +19,34 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public String createToken(LoginRequestDto loginRequestDto) {
+        Member member = memberRepository.findByLoginId(loginRequestDto.getLoginId())
+                .orElseThrow(IllegalArgumentException::new);
+        if (!loginRequestDto.getPassword().equals(member.getPassword())) { // 비밀번호 불일치 시
+            //TODO: 비밀번호 해시 함수를 사용해 암호화 해보기
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        } //TODO: 사용자 정의 예외 공부해보기
+        MemberLoginIdMainSubDto memberLoginIdMainSubDto = MemberLoginIdMainSubDto.of(member);
+        return jwtTokenProvider.createToken(memberLoginIdMainSubDto);
+    }
 
     public LocationTownDto getMainLocation(String memberLoginId) { // mainLocation 가져오는 메서드
         log.info("[MemberService.getMainLocation]");
-        Member member = memberRepository.findByLoginId(memberLoginId);
+        Member member = memberRepository.findByLoginId(memberLoginId).orElseThrow(IllegalArgumentException::new);
         return LocationTownDto.of(member.getMainLocation());
     }
 
     public LocationTownDto getSubLocation(String memberLoginId) { // subLocation 가져오는 메서드
         log.info("[MemberService.getSubLocation]");
-        Member member = memberRepository.findByLoginId(memberLoginId);
+        Member member = memberRepository.findByLoginId(memberLoginId).orElseThrow(IllegalArgumentException::new);
         return LocationTownDto.of(member.getSubLocation());
     }
 
     public MainSubTownDto getMainSubLocation(String memberLoginId) { // mainLocation, subLocation 동시에 가져오는 메서드
         log.info("[MemberService.getMainSubLocation]");
-        Member member = memberRepository.findByLoginId(memberLoginId);
+        Member member = memberRepository.findByLoginId(memberLoginId).orElseThrow(IllegalArgumentException::new);
         return MainSubTownDto.of(member);
     }
 }
