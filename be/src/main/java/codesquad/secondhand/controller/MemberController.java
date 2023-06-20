1e1ab2064440d2054b55ccbad59d9eed72f81659
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-21 03:56:43 +0900

    Feat: 기존 사용자의 로그인 기능 구현

--- be/src/main/java/codesquad/secondhand/controller/MemberController.java
+++ be/src/main/java/codesquad/secondhand/controller/MemberController.java
@@ -1,29 +1,40 @@
 package codesquad.secondhand.controller;
 
 import codesquad.secondhand.dto.ResponseDto;
+import codesquad.secondhand.dto.location.LocationTownDto;
 import codesquad.secondhand.dto.location.MainSubTownDto;
+import codesquad.secondhand.dto.member.LoginRequestDto;
+import codesquad.secondhand.dto.token.TokenResponse;
 import codesquad.secondhand.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 import static codesquad.secondhand.dto.StatusCode.RESPONSE_SUCCESS;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/{memberLoginId}")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/location")
-    public ResponseEntity<ResponseDto<MainSubTownDto>> showMemberLocations(@PathVariable String memberLoginId) {
+    @PostMapping("/login")
+    public ResponseEntity<ResponseDto<TokenResponse>> login(@RequestBody LoginRequestDto loginRequestDto) {
+        log.info("[MemberController.login]");
+        String token = memberService.createToken(loginRequestDto);
+        LocationTownDto mainLocation = memberService.getMainLocation(loginRequestDto.getLoginId());
+        LocationTownDto subLocation = memberService.getSubLocation(loginRequestDto.getLoginId());
+        return ResponseEntity.ok(ResponseDto.of(RESPONSE_SUCCESS, TokenResponse.of(token, mainLocation.getLocationIdx(), subLocation.getLocationIdx())));
+    }
+
+    @GetMapping("{memberLoginId}/location")
+    public ResponseEntity<ResponseDto<MainSubTownDto>> showMemberLocations(@PathVariable String memberLoginId, HttpServletRequest request) {
         log.info("[MemberController.showMemberLocations]");
+        Object name = (String) request.getAttribute("name");
         MainSubTownDto mainSubTownDto = memberService.getMainSubLocation(memberLoginId);
         return ResponseEntity.ok(ResponseDto.of(RESPONSE_SUCCESS, mainSubTownDto));
     }
