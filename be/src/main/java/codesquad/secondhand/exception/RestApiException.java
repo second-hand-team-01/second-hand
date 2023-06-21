90af91d6c40c292ead7ddeb0416ed3d52ed73d8d
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-22 04:16:37 +0900

    Feat: RestApiException 생성

---
+++ be/src/main/java/codesquad/secondhand/exception/RestApiException.java
@@ -0,0 +1,13 @@
+package codesquad.secondhand.exception;
+
+import codesquad.secondhand.exception.code.Code;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class RestApiException extends RuntimeException {
+
+    private final Code errorCode;
+
+}
