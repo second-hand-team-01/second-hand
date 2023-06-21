7129bf09ad83ff838c0f128103e32a4284ba9453
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-21 13:43:40 +0900

    Feat: 로그인 한 사용자의 정보를 조회하는 기능추가

--- be/src/main/java/codesquad/secondhand/controller/MemberController.java
+++ be/src/main/java/codesquad/secondhand/controller/MemberController.java
@@ -4,6 +4,7 @@ import codesquad.secondhand.dto.ResponseDto;
 import codesquad.secondhand.dto.location.LocationTownDto;
 import codesquad.secondhand.dto.location.MainSubTownDto;
 import codesquad.secondhand.dto.member.LoginRequestDto;
+import codesquad.secondhand.dto.member.MemberInfoDto;
 import codesquad.secondhand.dto.token.TokenResponse;
 import codesquad.secondhand.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -22,19 +23,26 @@ public class MemberController {
 
     private final MemberService memberService;
 
+//    @PostMapping("/join")
+//    public ResponseEntity<ResponseDto<>>
+
     @PostMapping("/login")
     public ResponseEntity<ResponseDto<TokenResponse>> login(@RequestBody LoginRequestDto loginRequestDto) {
         log.info("[MemberController.login]");
         String token = memberService.createToken(loginRequestDto);
-        LocationTownDto mainLocation = memberService.getMainLocation(loginRequestDto.getLoginId());
-        LocationTownDto subLocation = memberService.getSubLocation(loginRequestDto.getLoginId());
-        return ResponseEntity.ok(ResponseDto.of(RESPONSE_SUCCESS, TokenResponse.of(token, mainLocation.getLocationIdx(), subLocation.getLocationIdx())));
+        return ResponseEntity.ok(ResponseDto.of(RESPONSE_SUCCESS, TokenResponse.of(token, memberService.getMemberLoginIdMainSub(loginRequestDto.getLoginId()))));
+    }
+
+    @GetMapping("/info")
+    public ResponseEntity<ResponseDto<MemberInfoDto>> showMemberInfo(HttpServletRequest request) {
+        String loginId = (String) request.getAttribute("loginId");
+        MemberInfoDto memberInfo = memberService.getMemberInfo(loginId);
+        return ResponseEntity.ok(ResponseDto.of(RESPONSE_SUCCESS, memberInfo));
     }
 
     @GetMapping("{memberLoginId}/location")
     public ResponseEntity<ResponseDto<MainSubTownDto>> showMemberLocations(@PathVariable String memberLoginId, HttpServletRequest request) {
         log.info("[MemberController.showMemberLocations]");
-        Object name = (String) request.getAttribute("name");
         MainSubTownDto mainSubTownDto = memberService.getMainSubLocation(memberLoginId);
         return ResponseEntity.ok(ResponseDto.of(RESPONSE_SUCCESS, mainSubTownDto));
     }
