fe7cde6a780e081b16761dbff748e794c1ee7767
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-21 13:45:33 +0900

    Feat: WebMvcConfig 생성

---
+++ be/src/main/java/codesquad/secondhand/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package codesquad.secondhand.config;
+
+import codesquad.secondhand.interceptor.BearerAuthInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final BearerAuthInterceptor bearerAuthInterceptor;
+
+    public WebMvcConfig(BearerAuthInterceptor bearerAuthInterceptor) {
+        this.bearerAuthInterceptor = bearerAuthInterceptor;
+    }
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        log.info("[인터셉터 등록]");
+        registry.addInterceptor(bearerAuthInterceptor)
+                .addPathPatterns("/info");
+    }
+
+}
