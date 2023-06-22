5caad09a4b7c1edb0c10015ed14444448080d7a5
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-23 03:53:05 +0900

    Feat: NO_EXISTING_MEMBER 추가

--- be/src/main/java/codesquad/secondhand/exception/code/MemberErrorCode.java
+++ be/src/main/java/codesquad/secondhand/exception/code/MemberErrorCode.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum MemberErrorCode implements Code{
 
     AUTHENTICATION_INVALID_USER(false, 401, -40104, "이용할 수 없는 사용자입니다."),
+    NO_EXISTING_MEMBER(false, 404, -40401, "없는 사용자입니다."),
     SAME_ID_ALREADY_EXISTS(false, 400, -40000, "중복된 ID입니다."),
     REQUIRED_SIGNUP(false, 403, -40301, "회원가입이 필요한 사용자입니다."),
     WRONG_PASSWORD(false, 401, -40101, "비밀번호가 틀렸습니다.");
