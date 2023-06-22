ee2d5fdb8acd6ef41407c43842b5dec47ed067dd
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-22 12:55:43 +0900

    Feat: GlobalExceptionHandler 생성

---
+++ be/src/main/java/codesquad/secondhand/exception/GlobalExceptionHandler.java
@@ -0,0 +1,56 @@
+package codesquad.secondhand.exception;
+
+import codesquad.secondhand.exception.code.Code;
+import codesquad.secondhand.exception.code.CommonErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(RestApiException.class)
+    public ResponseEntity<Object> handleCustomException(RestApiException e) {
+        Code errorCode = e.getErrorCode();
+        log.error("RestApiException", e);
+        return handleExceptionInternal(errorCode);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgument(IllegalArgumentException e) {
+        log.error("handleIllegalArgument", e);
+        Code errorCode = CommonErrorCode.INVALID_PARAMETER;
+        return handleExceptionInternal(errorCode, e.getMessage());
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(Code errorCode) {
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(makeErrorResponse(errorCode));
+    }
+
+    private ErrorResponse makeErrorResponse(Code errorCode) {
+        return ErrorResponse.builder()
+                .success(errorCode.isSuccess())
+                .status(errorCode.getStatus())
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(Code errorCode, String message) {
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(makeErrorResponse(errorCode, message));
+    }
+
+    private ErrorResponse makeErrorResponse(Code errorCode, String message) {
+        return ErrorResponse.builder()
+                .success(errorCode.isSuccess())
+                .status(errorCode.getStatus())
+                .code(errorCode.getCode())
+                .message(message)
+                .build();
+    }
+}
