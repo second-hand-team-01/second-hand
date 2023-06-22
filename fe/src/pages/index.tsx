d0852e5b13a6f580ab699ef1b517eeed50c21c11
realsnoopso <real@snoop.so>
2023-06-22 17:25:04 +0900

    Fix: import 에러 수정

--- fe/src/pages/index.tsx
+++ fe/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { HomePage } from './HomePage/HomePage';
-import { WritePage } from './Write/WritePage';
+import { WritePage } from './WritePage/WritePage';
 import { LoginPage } from './LoginPage/LoginPage';
 
 export { HomePage, WritePage, LoginPage };
