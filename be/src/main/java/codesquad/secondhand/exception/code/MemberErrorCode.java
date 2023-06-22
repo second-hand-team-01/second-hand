0ba82178d0554f5ea4ec71c7effbc73c35338bf4
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-22 12:56:34 +0900

    Refactor: MemberErrorCode로 수정

--- be/src/main/java/codesquad/secondhand/exception/code/MemberErrorCode.java
+++ be/src/main/java/codesquad/secondhand/exception/code/MemberErrorCode.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum MemberCode implements Code{
+public enum MemberErrorCode implements Code{
 
     AUTHENTICATION_INVALID_USER(false, 401, -40104, "이용할 수 없는 사용자입니다."),
     REQUIRED_SIGNUP(false, 403, -40301, "추가 회원가입이 필요한 사용자입니다.");
