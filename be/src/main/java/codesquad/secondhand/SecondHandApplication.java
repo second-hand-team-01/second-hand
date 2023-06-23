e2a055a90310bfea3991218e4cbd752da3c7960f
realsnoopso <real@snoop.so>
2023-06-23 16:26:50 +0900

    Revert "Merge: Dev를 Fe와 합치기"
    This reverts commit b8cc340318a540a296af46050c5e7017cb6557c3.

--- be/src/main/java/codesquad/secondhand/SecondHandApplication.java
+++ be/src/main/java/codesquad/secondhand/SecondHandApplication.java
@@ -2,12 +2,14 @@ package codesquad.secondhand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SecondHandApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecondHandApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandApplication.class, args);
+    }
 
 }
--- be/src/main/java/codesquad/secondhand/config/JasyptConfig.java
+++ be/src/main/java/codesquad/secondhand/config/JasyptConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JasyptConfig {
-
     @Value("${jasypt.encryptor.password}")
     private String password;
 
