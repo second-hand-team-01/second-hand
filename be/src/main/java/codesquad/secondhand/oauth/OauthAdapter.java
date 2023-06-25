0c77e3da87b6e719566b5c2db043471bf9082e54
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-26 01:42:41 +0900

    Feat: OauthAdapter 생성

---
+++ be/src/main/java/codesquad/secondhand/oauth/OauthAdapter.java
@@ -0,0 +1,19 @@
+package codesquad.secondhand.oauth;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OauthAdapter {
+
+    private OauthAdapter() {}
+
+    // OauthProperties를 OauthProvider로 변환
+    public static Map<String, OauthProvider> getOauthProviders(OauthProperties properties) {
+        Map<String, OauthProvider> oauthProvider = new HashMap<>();
+
+        properties.getUser().forEach((key, value) -> oauthProvider.put(key, new OauthProvider(value, properties.getProvider().get(key))));
+
+        return oauthProvider;
+    }
+
+}
