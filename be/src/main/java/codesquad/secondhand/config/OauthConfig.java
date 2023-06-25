a5270cdae441d380313bb875a3b2fa9d2d947a9c
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-26 01:10:44 +0900

    Feat: 추후에 다른 OAuth 서비스 추가 시의 확장성을 고려한 코드 수정

--- be/src/main/java/codesquad/secondhand/config/OauthConfig.java
+++ be/src/main/java/codesquad/secondhand/config/OauthConfig.java
@@ -1,15 +1,28 @@
 package codesquad.secondhand.config;
 
+import codesquad.secondhand.oauth.InMemoryProviderRepository;
+import codesquad.secondhand.oauth.OauthAdapter;
 import codesquad.secondhand.oauth.OauthProperties;
-import lombok.RequiredArgsConstructor;
+import codesquad.secondhand.oauth.OauthProvider;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
 @Configuration
-@RequiredArgsConstructor
 @EnableConfigurationProperties(OauthProperties.class)
 public class OauthConfig {
 
     private final OauthProperties properties;
 
+    public OauthConfig(OauthProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    public InMemoryProviderRepository inMemoryProviderRepository() {
+        Map<String, OauthProvider> providers = OauthAdapter.getOauthProviders(properties);
+        return new InMemoryProviderRepository(providers);
+    }
 }
