3adb161b6a3f4ec494ebd669cbb3250716bc4a5a
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-26 01:43:39 +0900

    Feat: 추후 다른 OAuth 서비스 연동을 위한 확장성을 고려하여 코드 수정

--- be/src/main/java/codesquad/secondhand/oauth/OauthProvider.java
+++ be/src/main/java/codesquad/secondhand/oauth/OauthProvider.java
@@ -8,12 +8,13 @@ public class OauthProvider {
 
     private final String clientId;
     private final String clientSecret;
-    private final String redirectUrl;
-    private final String tokenUrl;
-    private final String userInfoUrl;
+    private String redirectUrl;
+    private String tokenUrl;
+    private String userInfoUrl;
 
     public OauthProvider(OauthProperties.User user, OauthProperties.Provider provider) {
-        this(user.getClientId(), user.getClientSecret(), user.getRedirectUri(), provider.getTokenUri(), provider.getUserInfoUri());
+        this(user.getClientId(), user.getClientSecret(), user.getRedirectUri(),
+                provider.getTokenUri(), provider.getUserInfoUri());
     }
 
     @Builder
