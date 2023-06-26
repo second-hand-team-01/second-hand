5b752edc3258c5a5a43ef7bba1fb3fdd4f8a9f59
lvalentine6 <ysr1227@naver.com>
2023-06-26 16:22:11 +0900

    Fix: log 추가

--- be/src/main/java/codesquad/secondhand/controller/OauthController.java
+++ be/src/main/java/codesquad/secondhand/controller/OauthController.java
@@ -1,26 +1,30 @@
 package codesquad.secondhand.controller;
 
-import codesquad.secondhand.dto.ResponseDto;
-import codesquad.secondhand.dto.oauth.OauthLoginResponse;
-import codesquad.secondhand.dto.token.TokenResponse;
-import codesquad.secondhand.service.OauthService;
-import lombok.RequiredArgsConstructor;
+import static codesquad.secondhand.exception.code.CommonResponseCode.*;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import static codesquad.secondhand.exception.code.CommonResponseCode.RESPONSE_SUCCESS;
+import codesquad.secondhand.dto.ResponseDto;
+import codesquad.secondhand.dto.oauth.OauthLoginResponse;
+import codesquad.secondhand.service.OauthService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class OauthController {
 
     private final OauthService oauthService;
 
     @GetMapping("/login/oauth/{provider}")
     public ResponseEntity<ResponseDto<OauthLoginResponse>> oauthLogin(@PathVariable String provider, @RequestParam String code) {
+        log.info("provider" + provider);
+        log.info("code" + code);
         OauthLoginResponse oauthLoginResponse = oauthService.oauthLogin(provider, code);
         return ResponseEntity.ok(ResponseDto.of(RESPONSE_SUCCESS, oauthLoginResponse));
     }
