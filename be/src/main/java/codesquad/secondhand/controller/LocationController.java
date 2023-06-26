d2000847b1532c1f1e82dfe4b0f3b20d69073553
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-26 16:23:49 +0900

    Refactor: 불필요한 import문 제거

--- be/src/main/java/codesquad/secondhand/controller/LocationController.java
+++ be/src/main/java/codesquad/secondhand/controller/LocationController.java
@@ -5,7 +5,6 @@ import codesquad.secondhand.dto.location.LocationDto;
 import codesquad.secondhand.service.LocationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- be/src/main/java/codesquad/secondhand/controller/MemberController.java
+++ be/src/main/java/codesquad/secondhand/controller/MemberController.java
@@ -8,7 +8,6 @@ import codesquad.secondhand.dto.token.TokenResponse;
 import codesquad.secondhand.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
