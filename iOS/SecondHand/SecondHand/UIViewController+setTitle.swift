e98619c85c65e02401c9b7c3303a6a7339f87517
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-06-26 15:27:08 +0900

    Feat: 뷰 컨트롤러 제목 설정 메소드 추가
    - 사용성을 위해 extension에 메소드 추가

---
+++ iOS/SecondHand/SecondHand/UIViewController+setTitle.swift
@@ -0,0 +1,14 @@
+//
+//  UIViewController+setTitle.swift
+//  SecondHand
+//
+//  Created by Wood on 2023/06/26.
+//
+
+import UIKit
+
+extension UIViewController {
+    func setTitle(_ title: String) {
+        self.title = "\(title)"
+    }
+}
