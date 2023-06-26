62b055550195b8ce1e8834e7eb2236772fd2db6c
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-26 16:47:27 +0900

    Feat: InMemoryProviderRepository 로그 추가

--- be/src/main/java/codesquad/secondhand/oauth/InMemoryProviderRepository.java
+++ be/src/main/java/codesquad/secondhand/oauth/InMemoryProviderRepository.java
@@ -1,15 +1,18 @@
 package codesquad.secondhand.oauth;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 
+@Slf4j
 @RequiredArgsConstructor
 public class InMemoryProviderRepository {
 
     private final Map<String, OauthProvider> providers;
 
     public OauthProvider findByProviderName(String name) {
+        log.info("[InMemoryProviderRepository]: {}", providers.get("github").getRedirectUrl());
         return providers.get(name);
     }
 }
