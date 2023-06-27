f12112be57a057886cb8a6ed69ecba0278e1c649
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-28 03:37:06 +0900

    Feat: 관심목록 API 구현을 위한 JPQL 추가

--- be/src/main/java/codesquad/secondhand/repository/ItemRepository.java
+++ be/src/main/java/codesquad/secondhand/repository/ItemRepository.java
@@ -1,5 +1,7 @@
 package codesquad.secondhand.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,6 +17,13 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 	@Query("SELECT i FROM Item i WHERE i.seller.memberIdx = :sellerIdx AND ("
 		+ "(i.status = '판매중' OR i.status = '예약중') AND :status = 'sell' "
 		+ "OR (i.status = '판매완료') AND :status = 'sold')")
-	Slice<Item> findBySellerIdxAndStatus(@Param("sellerIdx") Long sellerIdx, @Param("status") String status, @Param("pageable") Pageable pageable);
+	Slice<Item> findBySellerIdxAndStatus(@Param("sellerIdx") Long sellerIdx, @Param("status") String status,
+		@Param("pageable") Pageable pageable);
+
+	@Query("SELECT DISTINCT i.item.category.categoryIdx FROM Interest i WHERE i.member.memberIdx = :memberIdx ORDER BY i.item.category.categoryIdx ASC")
+	List<Long> findDistinctCategoryIdxByMemberIdx(@Param("memberIdx") Long memberIdx);
+
+	@Query("SELECT i FROM Item i WHERE i.itemIdx IN (SELECT interest.item.itemIdx FROM Interest interest WHERE interest.member.memberIdx = :memberIdx)")
+	Slice<Item> findItemsBySellerInterest(@Param("memberIdx") Long memberIdx, Pageable pageable);
 
 }
