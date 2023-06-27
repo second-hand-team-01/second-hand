899b130a1b7859965137c46acb72a7fdc732fcf4
poco <godiva140@gmail.com>
2023-06-27 14:14:22 +0900

    Feat: locationSelector 추가

--- fe/src/components/commons/LocationSelector/LocationSelectorStyle.ts
+++ fe/src/components/commons/LocationSelector/LocationSelectorStyle.ts
@@ -4,12 +4,4 @@ export const LocationSelectorStyle = styled.div`
   padding: 40px 0;
 `;
 
-export const LocationContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  justify-items: center;
-  row-gap: 32px;
-  font-size: ${({ theme }) => theme.typography.footnote.size};
-  font-weight: ${({ theme }) => theme.fontWeights.light};
-  line-height: ${({ theme }) => theme.typography.footnote.lineHeight};
-`;
+export const LocationContainer = styled.div``;
