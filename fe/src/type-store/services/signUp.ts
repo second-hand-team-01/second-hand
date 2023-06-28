206fc3eed17e3ade1436e57aab9dc7cb06470195
poco <godiva140@gmail.com>
2023-06-29 03:01:24 +0900

    Feat: signUp 페이지 props 타입 수정

--- fe/src/type-store/services/signUp.ts
+++ fe/src/type-store/services/signUp.ts
@@ -3,21 +3,32 @@ export interface State {
   password: { value: string; isValid: boolean | null; isTouched?: boolean };
   mainLocation: {
     locationIdx: number | null;
-    locationName: string | null;
+    town: string | null;
   };
   subLocation: {
     locationIdx: number | null;
-    locationName: string | null;
+    town: string | null;
   };
   imgUrl: string | null;
+  imgFile: string | null;
 }
 
-interface LocationInfo {
+export interface LocationInfo {
   locationIdx: number;
-  locationName: string;
+  locationName?: string;
+  town?: string;
 }
 
+// TODO: any 타입 삭제
 export interface Action {
   type: string;
-  val: string | LocationInfo;
+  val: string | LocationInfo | any;
 }
+
+export type LocationDataType = {
+  locationIdx: number;
+  locationName: string;
+  city: string;
+  district: string;
+  town: string;
+};
