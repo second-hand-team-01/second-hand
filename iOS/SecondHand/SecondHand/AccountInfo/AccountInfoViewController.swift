ea13f55387c4fbaf4414bfca099859eb97796d44
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-06-28 14:33:21 +0900

    Feat: 로그아웃 버튼 기능 추가
    - Action을 뷰컨에서 바로 지정하기 위해 버튼 위치 AccountInfoView > AccountInfoVC 이동
    - AccountInfoVC에 Constraint 설정 메소드 추가
    - AccountInfoVC에 뒤로가기 버튼 숨김 처

--- iOS/SecondHand/SecondHand/AccountInfo/AccountInfoViewController.swift
+++ iOS/SecondHand/SecondHand/AccountInfo/AccountInfoViewController.swift
@@ -9,28 +9,69 @@ import UIKit
 
 class AccountInfoViewController: UIViewController {
     private var accountInfoView = AccountInfoView()
+    private var signOutButton: UIButton = {
+        let button = UIButton()
+        button.titleLabel?.font = .typo.subHead
+        button.tintColor = .white
+        button.setTitle("로그아웃", for: .normal)
+        button.layer.cornerRadius = 8
+        button.backgroundColor = .orange
+        
+        var configuration = UIButton.Configuration.plain()
+        configuration.contentInsets = NSDirectionalEdgeInsets(
+            top: 16,
+            leading: 20,
+            bottom: 16,
+            trailing: 20
+        )
+        button.configuration = configuration
+        return button
+    }()
     var userName: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.addSubviews()
         self.accountInfoView.configure(userName: userName)
         self.navigationItem.hidesBackButton = true
+        signOutButton.addTarget(
+            self,
+            action: #selector(didTapSignOutButton),
+            for: .touchUpInside
+        )
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        layoutAccountInfoView()
+        addConstraints()
+    }
+    
+    @objc private func didTapSignOutButton() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    func addConstraints() {
+        self.layoutAccountInfoView()
+        self.addConstraintToSignOutButton()
     }
 }
 
 // MARK: - Cosntraint 설정 메소드
 extension AccountInfoViewController {
-    private func layoutAccountInfoView() {
-        self.accountInfoView.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(accountInfoView)
+    private func addSubviews() {
+        let subViews = [
+            self.accountInfoView,
+            self.signOutButton
+        ]
         
-        guard let navigationBarBottomAnchor = self.navigationController?.navigationBar.bottomAnchor,
-              let tabBarTopAnchor = self.tabBarController?.tabBar.topAnchor else { return }
+        subViews.forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview($0)
+        }
+    }
+    
+    private func layoutAccountInfoView() {
+        guard let navigationBarBottomAnchor = self.navigationController?.navigationBar.bottomAnchor else { return }
         let leadingConstraint: CGFloat = 16
         let trailingConstraint: CGFloat = -16
         let topConstraint: CGFloat = 96
@@ -48,7 +89,21 @@ extension AccountInfoViewController {
                 equalTo: navigationBarBottomAnchor,
                 constant: topConstraint
             ),
-            self.accountInfoView.bottomAnchor.constraint(greaterThanOrEqualTo: tabBarTopAnchor)
+        ])
+    }
+    
+    private func addConstraintToSignOutButton() {
+        let topConstraint: CGFloat = 276
+        guard let tabBarTopAnchor = self.tabBarController?.tabBar.topAnchor else { return }
+        
+        NSLayoutConstraint.activate([
+            self.signOutButton.leadingAnchor.constraint(equalTo: self.accountInfoView.leadingAnchor),
+            self.signOutButton.trailingAnchor.constraint(equalTo: self.accountInfoView.trailingAnchor),
+            self.signOutButton.topAnchor.constraint(
+                equalTo: self.accountInfoView.bottomAnchor,
+                constant: topConstraint
+            ),
+            self.signOutButton.bottomAnchor.constraint(greaterThanOrEqualTo: tabBarTopAnchor)
         ])
     }
 }
