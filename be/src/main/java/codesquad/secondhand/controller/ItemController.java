c5f18636a8c439f910117059da94f276e419314a
lvalentine6 <ysr1227@naver.com>
2023-06-28 19:56:44 +0900

    Feat: item save 기능 구현

--- be/src/main/java/codesquad/secondhand/controller/ItemController.java
+++ be/src/main/java/codesquad/secondhand/controller/ItemController.java
@@ -6,10 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import codesquad.secondhand.dto.ResponseDto;
 import codesquad.secondhand.dto.item.ItemDetailDto;
+import codesquad.secondhand.dto.item.ItemIdxDto;
 import codesquad.secondhand.dto.item.ItemSliceDto;
 import codesquad.secondhand.service.ItemService;
 import lombok.RequiredArgsConstructor;
@@ -48,19 +47,20 @@ public class ItemController {
 	}
 
 	@PostMapping
-	public ResponseDto<ItemSliceDto> addItem(HttpServletRequest httpServletRequest,
+	public ResponseDto<ItemIdxDto> addItem(HttpServletRequest httpServletRequest,
 		@ModelAttribute ItemDetailDto itemDetailDto) {
 		Long memberIdx = (Long)httpServletRequest.getAttribute("memberIdx");
 		itemDetailDto.setSellerIdx(memberIdx);
-		itemService.creatItem(itemDetailDto);
+		ItemIdxDto itemIdxDto = itemService.creatItem(itemDetailDto);
+		return ResponseDto.of(RESPONSE_SUCCESS, itemIdxDto);
+	}
+
+	@GetMapping
+	// TODO: 조회수
+	public ResponseDto<ItemSliceDto> showItemDetail() {
 		return ResponseDto.of(RESPONSE_SUCCESS, null);
 	}
 
-	// @GetMapping
-	// public ResponseDto<ItemSliceDto> showItemDetail() {
-	// 	return ResponseDto.of(RESPONSE_SUCCESS, null);
-	// }
-	//
 	// @PatchMapping
 	// public ResponseDto<ItemSliceDto> editItemDetail() {
 	// 	return ResponseDto.of(RESPONSE_SUCCESS, null);
