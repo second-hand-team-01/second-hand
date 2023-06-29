fbecca37a9a27646b612905110fca5f53c5bcf51
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-06-30 08:02:03 +0900

    Feat: 다른 사람의 게시글을 수정하려 할 때 사용할 에러 코드 추가

--- be/src/main/java/codesquad/secondhand/exception/code/ItemErrorCode.java
+++ be/src/main/java/codesquad/secondhand/exception/code/ItemErrorCode.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ItemErrorCode implements Code {
-	UnauthorizedException(false, 401, -40001, "다른 사람의 게시글은 삭제할 수 없습니다."),
-
+	UNAUTHORIZED_EXCEPTION_DELETE(false, 401, -40001, "다른 사람의 게시글은 삭제할 수 없습니다."),
+	UNAUTHORIZED_EXCEPTION_EDIT(false, 401, -40001, "다른 사람의 게시글은 수정할 수 없습니다."),
 	NO_EXISTING_ITEM(false, 404, -40401, "없는 상품입니다.");
 
 	private final boolean success;
