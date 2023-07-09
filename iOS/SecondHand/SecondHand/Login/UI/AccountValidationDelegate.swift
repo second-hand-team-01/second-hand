ab4dc846b2d6e09fdb2e612e45fc553bb68fd8e7
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-07-09 14:44:27 +0900

    Refactor: 로그인 입력분기 기능 리팩토링
    - 가독성을 고려하여 분기처리 if > guard 대체
    - 테스트 편리성을 위해 메소드 형태로 작게 분할

---
+++ iOS/SecondHand/SecondHand/Login/UI/AccountValidationDelegate.swift
@@ -0,0 +1,69 @@
+//
+//  AccountValidationDelegate.swift
+//  SecondHand
+//
+//  Created by Wood on 2023/07/02.
+//
+
+import UIKit
+import RegexBuilder
+
+class AccountValidationDelegate: NSObject, UITextFieldDelegate {
+    private let inputLimitRange: ClosedRange = (3...12)
+    private let regexPattern: String = "[^A-Z|a-z|0-9]"
+    var isValidSender: ((Bool) -> ())?
+
+    func isUserTypingDelete(length: Int) -> Bool {
+        guard length == 0 else {
+            return false
+        }
+        
+        return true
+    }
+    
+    func validateLength(location: Int) -> Bool {
+        guard inputLimitRange.contains(location) else {
+            return false
+        }
+
+        return true
+    }
+    
+    func validateId(
+        by text: String,
+        with location: Int
+    ) -> Bool {
+        guard text.range(of: regexPattern, options: .regularExpression) == nil else {
+            return false
+        }
+        
+        return true
+    }
+    
+    func textField(
+        _ textField: UITextField,
+        shouldChangeCharactersIn range: NSRange,
+        replacementString string: String)
+    -> Bool {
+        var location = range.location + 1
+        var text = (textField.text ?? "") + string
+        
+        if !isUserTypingDelete(length: range.length) {
+            location -= 1
+            text = String(text.dropLast(1))
+        }
+        
+        guard validateLength(location: location) else {
+            isValidSender?(false)
+            return true
+        }
+        
+        guard validateId(by: text, with: location) else {
+            isValidSender?(false)
+            return true
+        }
+        
+        isValidSender?(true)
+        return true
+    }
+}
