ea4959b869a16e7ef29508afc73a1be3c33817dd
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-07-17 14:23:53 +0900

    Fix: 제품 상세정보 모델 수정
    - 누락된 화면에 표시할 데이터 변수들 추가

--- iOS/SecondHand/SecondHand/Detail/DetailModel.swift
+++ iOS/SecondHand/SecondHand/Detail/DetailModel.swift
@@ -11,29 +11,38 @@ class DetailModel {
     struct ProductInfo {
         let title: String
         let category: String
-        let updatedTime: String
+        let postedTime: String
         let description: String
     }
     
-    struct Count {
+    struct UserInteractionCount {
         let chat: Int
         let favorite: Int
         let view: Int
     }
 
-    let imageKeys: [String]
+    let imageKeys: [NSString?]
     let sellerName: String
+    let productInfo: ProductInfo
+    let userInteractionCount: UserInteractionCount
+    let price: Int
     var status: String
     var isUserInterested: Bool
     
     init(
-        imageKeys: [String],
+        imageKeys: [NSString?],
         sellerName: String,
+        productInfo: ProductInfo,
+        userInteractionCount: UserInteractionCount,
+        price: Int,
         status: String,
         isUserInterested: Bool
     ) {
         self.imageKeys = imageKeys
         self.sellerName = sellerName
+        self.productInfo = productInfo
+        self.userInteractionCount = userInteractionCount
+        self.price = price
         self.status = status
         self.isUserInterested = isUserInterested
     }
