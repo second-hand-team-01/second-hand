70eb9767c7bc69068bd9eae68a421458ccdc02a1
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-07-17 15:17:36 +0900

    Feat: 캐싱된 이미지 URL 모델 매핑 후 반환하는 로직 추가
    - 모든 이미지가 메모리 캐시에 존재할 때 모델 반환
    - ImageCacheManager에 캐싱된 이미지 숫자를 담는 Cache 변수 추가
    - 기존 URL 담는 캐시 변수 네이밍 수정

--- iOS/SecondHand/SecondHand/Detail/DetailRepository.swift
+++ iOS/SecondHand/SecondHand/Detail/DetailRepository.swift
@@ -10,14 +10,13 @@ import Foundation
 struct DetailRepository {
     private var remoteDataSource: DetailRemoteDataSource?
     private var localDataSource: DetailLocalDataSource?
+    private var detailModelMapper: DetailModelMapper?
     
-    private func compare(images: [Data?]) {
+    private func validateImages(data: ItemDetailDTO) -> Bool {
         
     }
     
     func fetchData(item index: Int) async -> DetailModel? {
-        let path = NSString(string: "\(index)")
-        
         // 1. Network로부터 데이터를 가져온다.
         guard let fetchedData = await remoteDataSource?.request(item: index)?.data else {
             LogManger.generate(level: .network, LogMessage.failToLoadData)
@@ -25,11 +24,33 @@ struct DetailRepository {
         }
 
         // 서버에 이미지가 없는지 확인
+        let imageKey = NSString(string: "\(index)")
         guard !fetchedData.imageUrl.isEmpty else {
             LogManger.generate(level: .network, LogMessage.imageDidNotExisted)
-            return convertToModel(by: fetchedData, with: [])
+            return detailModelMapper?.convert(by: fetchedData, with: [])
+        }
+
+        // 2. 캐싱된 이미지 수와 동일한지 확인
+        guard let cachedImagesCount = ImageCacheManager.counter.object(forKey: imageKey)?.intValue as? Int else {
+            return detailModelMapper?.convert(by: fetchedData, with: [])
         }
         
+        guard fetchedData.imageUrl.count != cachedImagesCount else {
+            let imageCaches = (0..<cachedImagesCount).map { (imageIndex: Int) in
+                return NSString(string: "\(index)/\(imageIndex)")
+            }
+
+            return detailModelMapper?.convert(by: fetchedData, with: imageCaches)
+        }
+        
+        // 3. 디스크 캐시에 이미지 존재하는지 확인
+        
+//        let imageURLs = (0..<cachedImagesCount).map { (imageIndex: Int) in
+//            let key = NSString(string: "\(index)/\(imageIndex)")
+//            return ImageCacheManager.saver.object(forKey: key)
+//        }
+    }
+    
     enum LogMessage {
         static let failToLoadData = "데이터를 네트워크로부터 가져오는데 실패했습니다."
         static let imageDidNotExisted = "이미지가 로드되지 않았습니다."
--- iOS/SecondHand/SecondHand/ImageCacheManager.swift
+++ iOS/SecondHand/SecondHand/ImageCacheManager.swift
@@ -8,6 +8,18 @@
 import Foundation
 
 struct ImageCacheManager {
-    static let shared = NSCache<NSString, NSString>()
+    static let counter = NSCache<NSString, NSString>()
+    static let saver = NSCache<NSString, NSURL>()
     private init() {}
+    
+    static func check(key: String) -> Bool {
+        if let isExist = self.saver.object(
+            forKey: NSString(
+                string: key
+            )
+        ) {
+            return true
+        }
+        return false
+    }
 }
