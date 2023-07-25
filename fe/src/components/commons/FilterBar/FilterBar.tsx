193993e9f1a337476a3865b54d03900b4c4a8db3
poco <godiva140@gmail.com>
2023-07-25 14:27:25 +0900

    Refactor: 지역 설정 팝업 수정

--- fe/src/components/commons/FilterBar/FilterBar.tsx
+++ fe/src/components/commons/FilterBar/FilterBar.tsx
@@ -2,7 +2,7 @@ import * as S from './FilterBarStyle';
 import { Button, Dropdown } from '@commons/index';
 import { Category } from '@type-store/services/category';
 import { MenuButtonProps } from '@components/commons/Menu/MenuStyle';
-
+import { useState, useEffect } from 'react';
 
 export interface FilterBarProps {
   mainLocation?: string;
@@ -13,6 +13,7 @@ export interface FilterBarProps {
   selectedCategory?: Category;
   handleDeleteBtn?: (e: React.MouseEvent<HTMLButtonElement>) => void;
   openState?: [boolean, React.Dispatch<React.SetStateAction<boolean>>];
+  locationPopupHandler?: any;
 }
 
 export const FilterBar = ({
@@ -23,54 +24,46 @@ export const FilterBar = ({
   openState,
   mainLocation,
   subLocation,
+  locationPopupHandler,
 }: FilterBarProps) => {
   if (!openState) return <></>;
 
   const [isOpen, setOpen] = openState;
 
-  // const menuButtonPropsList = [
-  //   {
-  //     shape: 'small',
-  //     state: 'default',
-  //     name: mainLocation,
-  //     onClick: () => setOpen(false),
-  //   },
-  //   subLocation
-  //     ? [
-  //         {
-  //           shape: 'small',
-  //           state: 'default',
-  //           name: subLocation,
-  //           onClick: () => setOpen(false),
-  //         },
-  //       ]
-  //     : [],
-  //   {
-  //     shape: 'small',
-  //     state: 'default',
-  //     name: '내동네 설정하기',
-  //     onClick: () => setOpen(false),
-  //   },
-  // ];
+  const [menuButtonPropsList, setMenuButtonPropsList] = useState<
+    MenuButtonProps[]
+  >([
+    {
+      shape: 'small',
+      state: 'default',
+      name: mainLocation,
+      onClick: () => setOpen(false),
+    },
+
+    {
+      shape: 'small',
+      state: 'default',
+      name: '내동네 설정하기',
+      onClick: () => locationPopupHandler(),
+    },
+  ]);
+
+  useEffect(() => {
+    const newMenu: MenuButtonProps = {
+      shape: 'small',
+      state: 'default',
+      name: subLocation,
+      onClick: () => () => setOpen(false),
+    };
+    if (subLocation) {
+      setMenuButtonPropsList((prev) => [prev[0], newMenu, prev[1]]);
+    }
+  }, [subLocation]);
 
   return (
     <S.FilterBar selectedCategory={selectedCategory}>
       <Dropdown
-        menuButtonPropsList={[
-          {
-            shape: 'small',
-            state: 'default',
-            name: 'ddd',
-            onClick: () => setOpen(false),
-          },
-          {
-            shape: 'small',
-            state: 'default',
-            name: 'ddd2',
-            onClick: () => setOpen(false),
-          },
-        ]}
-        // menuButtonPropsList={menuButtonPropsList}
+        menuButtonPropsList={menuButtonPropsList}
         openState={[isOpen, setOpen]}
         onClick={() => setOpen(true)}
       >
