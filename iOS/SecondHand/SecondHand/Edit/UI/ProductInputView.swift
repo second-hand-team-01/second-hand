d09b0efc98bac1d6161f3738e9f477ae2385fb73
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-08-14 20:40:29 +0900

    Design: TextField 디자인 수정
    - placeholder 추가
    - placeholder에 추가할 텍스트들 타입으로 분리
    - placeholder 2줄 이상으로 적용하기 위해 설명입력란 TextField > TextView로 변경

--- iOS/SecondHand/SecondHand/Edit/UI/ProductInputView.swift
+++ iOS/SecondHand/SecondHand/Edit/UI/ProductInputView.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 
-final class ProductInputView: UIView {
-    private var titleTextField = UITextField()
+final class ProductInputView: UIView, UITextViewDelegate {
+    private var titleTextField: UITextField = {
+        var textField = UITextField()
+        textField.placeholder = Content.Placeholder.title
+        return textField
+    }()
     private var categoryStackView: UIStackView = {
         var stackView = UIStackView()
         stackView.spacing = 4
@@ -17,7 +21,7 @@ final class ProductInputView: UIView {
     private var categoryListButton: UIButton = {
         var button = UIButton()
         button.setImage(
-            UIImage(systemName: "chevron.right"),
+            UIImage(systemName: Content.ImageName.categoryButton),
             for: .normal
         )
         button.tintColor = .black
@@ -28,23 +32,48 @@ final class ProductInputView: UIView {
         view.backgroundColor = UIColor.lightGray
         return view
     }()
-    private var priceTextField = UITextField()
+    private var priceTextField: UITextField = {
+       var textField = UITextField()
+        textField.placeholder = Content.Placeholder.price
+        return textField
+    }()
     private var secondBottomLine: UIView = {
         var view = UIView()
         view.backgroundColor = UIColor.lightGray
         return view
     }()
-    private var descriptionTextField = UITextField()
+    private var descriptionTextView: UITextView = {
+        var textView = UITextView()
+        textView.textColor = .lightGray
+        textView.text = Content.Placeholder.description
+        return textView
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.setDelegateToDescriptionTextView()
         self.addSubviews()
         self.addConstraints()
-        self.titleTextField.text = "123"
-        self.priceTextField.text = "100,000"
-        self.descriptionTextField.text = "판매중이다"
+    }
+    
+    // MARK: - Description TextView
+    
+    private func setDelegateToDescriptionTextView() {
+        self.descriptionTextView.delegate = self
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = nil
+        textView.textColor = .black
     }
 
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text == nil {
+            textView.textColor = .lightGray
+            textView.text = Content.Placeholder.description
+        }
+    }
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
@@ -57,7 +86,7 @@ final class ProductInputView: UIView {
             self.firstBottomLine,
             self.priceTextField,
             self.secondBottomLine,
-            self.descriptionTextField
+            self.descriptionTextView
         ]
 
         subViews.forEach {
@@ -74,7 +103,7 @@ final class ProductInputView: UIView {
         self.addConstraintToFirstBottomLine()
         self.addConstraintToPriceTextField()
         self.addConstraintToSecndBottomLine()
-        self.addConstraintToDescriptionTextField()
+        self.addConstraintToDescriptionTextView()
     }
     
     private func addConstraintToTitleTextField() {
@@ -91,8 +120,8 @@ final class ProductInputView: UIView {
                 equalTo: self.titleTextField.bottomAnchor,
                 constant: 8
             ),
-            self.categoryStackView.leadingAnchor.constraint(
-                equalTo: self.leadingAnchor)
+            self.categoryStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.categoryStackView.heightAnchor.constraint(equalToConstant: 32)
         ])
     }
     
@@ -112,13 +141,13 @@ final class ProductInputView: UIView {
     
     private func addConstraintToFirstBottomLine() {
         NSLayoutConstraint.activate([
-            firstBottomLine.topAnchor.constraint(
+            self.firstBottomLine.topAnchor.constraint(
                 equalTo: self.categoryStackView.bottomAnchor,
                 constant: 15
             ),
-            firstBottomLine.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            firstBottomLine.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            firstBottomLine.heightAnchor.constraint(equalToConstant: 1.0)
+            self.firstBottomLine.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.firstBottomLine.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.firstBottomLine.heightAnchor.constraint(equalToConstant: 1.0)
         ])
     }
     
@@ -145,15 +174,26 @@ final class ProductInputView: UIView {
         ])
     }
     
-    private func addConstraintToDescriptionTextField() {
+    private func addConstraintToDescriptionTextView() {
         NSLayoutConstraint.activate([
-            self.descriptionTextField.topAnchor.constraint(
+            self.descriptionTextView.topAnchor.constraint(
                 equalTo: self.secondBottomLine.bottomAnchor,
                 constant: 15
             ),
-            self.descriptionTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            self.descriptionTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            self.descriptionTextField.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+            self.descriptionTextView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.descriptionTextView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.descriptionTextView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
         ])
+    }
+    
+    enum Content {
+        struct Placeholder {
+            static let title = "글 제목"
+            static let price = "₩ 가격(선택사항)"
+            static let description = "게시물 내용을 작성해주세요.(판매금지 물품은 게시가 제한될 수 있어요.)"
+        }
+        struct ImageName {
+            static let categoryButton = "chevron.right"
+        }
     }
 }
