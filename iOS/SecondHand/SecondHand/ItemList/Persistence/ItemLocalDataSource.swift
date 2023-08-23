87b4a80ec443386b851659dbdcfa3bd0247a8ce8
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-08-23 14:35:14 +0900

    FEAT: 상품 이미지 존재 여부 확인하는 메소드 추가

--- iOS/SecondHand/SecondHand/ItemList/Persistence/ItemLocalDataSource.swift
+++ iOS/SecondHand/SecondHand/ItemList/Persistence/ItemLocalDataSource.swift
@@ -8,10 +8,29 @@
 import Foundation
 
 protocol ListLocalDataSource {
+    func checkFileExists(itemIndex: Int) -> Bool
     func fetchImageKeys(itemIndex: Int, imageUrl: String) -> NSString
 }
 
 struct ItemListLocalDataSource: ListLocalDataSource {
+    private var fileManager = FileManager.default
+    private let cacheDirectoryPath: String = {
+        let cacheDirectoryURL = FileManager.default.urls(
+            for: .cachesDirectory,
+            in: .userDomainMask
+        )[0]
+        return cacheDirectoryURL.path()
+    }()
+    
+    func checkFileExists(itemIndex: Int) -> Bool {
+        let path = "\(self.cacheDirectoryPath)/ItemList/\(itemIndex).jpeg"
+
+        if self.fileManager.fileExists(atPath: path) {
+            return true
+        }
+        return false
+    }
+    
     func fetchImageKeys(itemIndex: Int, imageUrl: String) -> NSString {
         
     }
